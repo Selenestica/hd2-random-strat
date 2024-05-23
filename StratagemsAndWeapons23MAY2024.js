3c0581b6c602a3567e6266ad2aaf14aa9ac24bf5
tylerjohnsondagoat <tlbaker27@gmail.com>
2024-05-23 15:33:25 -0400

    Added .csv and .js files for strats + weapons
    Added current working files with all internal names. Currently still need to update all image names to match InternalName, and then update this .csv and .js file.

---
+++ StratagemsAndWeapons23MAY2024.js
@@ -0,0 +1,767 @@
+[
+  {
+    "Display Name": "Airburst Rocket Launcher",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Weapons",
+    "Warbond": "",
+    "InternalName": "rl77airburstrocketlauncher",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Autocannon",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Weapons",
+    "Warbond": "",
+    "InternalName": "ac8autocannon",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Expendable Anti-Tank",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Weapons",
+    "Warbond": "",
+    "InternalName": "eat17expendableantitank",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Flamethrower",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Weapons",
+    "Warbond": "",
+    "InternalName": "flam40flamethrower",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Laser Cannon",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Weapons",
+    "Warbond": "",
+    "InternalName": "las98lasercannon",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Stalwart",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Weapons",
+    "Warbond": "",
+    "InternalName": "m105stalwart",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Machine Gun",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Weapons",
+    "Warbond": "",
+    "InternalName": "mg43machinegun",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Arc Thrower",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Weapons",
+    "Warbond": "",
+    "InternalName": "arc3arcthrower",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Grenade Launcher",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Weapons",
+    "Warbond": "",
+    "InternalName": "gl21grenadelauncher",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Anti-Material Rifle",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Weapons",
+    "Warbond": "",
+    "InternalName": "apw1antimaterielrifle",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Railgun",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Weapons",
+    "Warbond": "",
+    "InternalName": "rs422railgun",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Recoilless Rifle",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Weapons",
+    "Warbond": "",
+    "InternalName": "gr8recoillessrifle",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Spear",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Weapons",
+    "Warbond": "",
+    "InternalName": "faf14spear",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Quasar Cannon",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Weapons",
+    "Warbond": "",
+    "InternalName": "las99quasarcannon",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Heavy Machine Gun",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Weapons",
+    "Warbond": "",
+    "InternalName": "mg206heavymachinegun",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Guard Dog Laser",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Backpacks",
+    "Warbond": "",
+    "InternalName": "axlas5guarddogrover",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Guard Dog Bullets",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Backpacks",
+    "Warbond": "",
+    "InternalName": "ad334guarddog",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Jump Pack",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Backpacks",
+    "Warbond": "",
+    "InternalName": "lift850jumppack",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Supply Pack",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Backpacks",
+    "Warbond": "",
+    "InternalName": "b1supplypack",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Shield Generator Pack",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Backpacks",
+    "Warbond": "",
+    "InternalName": "sh20shieldgeneratorpack",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Ballistic Shield Backpack",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Backpacks",
+    "Warbond": "",
+    "InternalName": "sh20ballisticshieldbackpack",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Patriot Exosuit",
+    "Type": "Stratagem",
+    "Category": "Supply",
+    "Subcategory": "Vehicles",
+    "Warbond": "",
+    "InternalName": "exo45patriotexosuit",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Eagle Strafing Run",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Eagle",
+    "Warbond": "",
+    "InternalName": "eaglestrafingrun",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Eagle Airstrike",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Eagle",
+    "Warbond": "",
+    "InternalName": "eagleairstrike",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Eagle Cluster Bomb",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Eagle",
+    "Warbond": "",
+    "InternalName": "eagleclusterbomb",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Eagle Napalm Strike",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Eagle",
+    "Warbond": "",
+    "InternalName": "eaglenapalmstrike",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Eagle Smoke Strike",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Eagle",
+    "Warbond": "",
+    "InternalName": "eaglesmokestrike",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Eagle 110mm Rocket Pods",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Eagle",
+    "Warbond": "",
+    "InternalName": "eagle110mmrocketpods",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Eagle 500kg Bomb",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Eagle",
+    "Warbond": "",
+    "InternalName": "eagle500kgbomb",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Orbital Precision Strike",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Orbital",
+    "Warbond": "",
+    "InternalName": "orbitalprecisionstrike",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Orbital Airburst Strike",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Orbital",
+    "Warbond": "",
+    "InternalName": "orbitalairburststrike",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Orbital 120mm HE Barrage",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Orbital",
+    "Warbond": "",
+    "InternalName": "orbital120mmhebarrage",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Orbital 380mm HR Barrage",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Orbital",
+    "Warbond": "",
+    "InternalName": "orbital380mmhrbarrage",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Orbital Walking Barrage",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Orbital",
+    "Warbond": "",
+    "InternalName": "orbitalwalkingbarrage",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Orbital Laser",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Orbital",
+    "Warbond": "",
+    "InternalName": "orbitallaser",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Orbital Railcannon Strike",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Orbital",
+    "Warbond": "",
+    "InternalName": "orbitalrailcannonstrike",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Orbital Gatling Barrage",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Orbital",
+    "Warbond": "",
+    "InternalName": "orbitalgatlingbarrage",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Orbital Gas Strike",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Orbital",
+    "Warbond": "",
+    "InternalName": "orbitalgasstrike",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Orbital EMS Strike",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Orbital",
+    "Warbond": "",
+    "InternalName": "orbitalemsstrike",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Orbital Smoke Strike",
+    "Type": "Stratagem",
+    "Category": "Offensive",
+    "Subcategory": "Orbital",
+    "Warbond": "",
+    "InternalName": "orbitalsmokestrike",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Tesla Tower",
+    "Type": "Stratagem",
+    "Category": "Defensive",
+    "Subcategory": "Emplacement",
+    "Warbond": "",
+    "InternalName": "teslatower",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Mortar Sentry",
+    "Type": "Stratagem",
+    "Category": "Defensive",
+    "Subcategory": "Sentry",
+    "Warbond": "",
+    "InternalName": "mortarsentry",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "EMS Mortar Sentry",
+    "Type": "Stratagem",
+    "Category": "Defensive",
+    "Subcategory": "Sentry",
+    "Warbond": "",
+    "InternalName": "emsmortarsentry",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Machine Gun Sentry",
+    "Type": "Stratagem",
+    "Category": "Defensive",
+    "Subcategory": "Sentry",
+    "Warbond": "",
+    "InternalName": "machinegunsentry",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Gatling Sentry",
+    "Type": "Stratagem",
+    "Category": "Defensive",
+    "Subcategory": "Sentry",
+    "Warbond": "",
+    "InternalName": "gatlingsentry",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Anti-Personnel Minefield",
+    "Type": "Stratagem",
+    "Category": "Defensive",
+    "Subcategory": "Emplacement",
+    "Warbond": "",
+    "InternalName": "anti-personnelminefield",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Incendiary Mines",
+    "Type": "Stratagem",
+    "Category": "Defensive",
+    "Subcategory": "Emplacement",
+    "Warbond": "",
+    "InternalName": "incendiarymines",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Shield Generator Relay",
+    "Type": "Stratagem",
+    "Category": "Defensive",
+    "Subcategory": "Emplacement",
+    "Warbond": "",
+    "InternalName": "shieldgeneratorrelay",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "HMG Emplacement",
+    "Type": "Stratagem",
+    "Category": "Defensive",
+    "Subcategory": "Emplacement",
+    "Warbond": "",
+    "InternalName": "hmgemplacement",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Autocannon Sentry",
+    "Type": "Stratagem",
+    "Category": "Defensive",
+    "Subcategory": "Sentry",
+    "Warbond": "",
+    "InternalName": "autocannonsentry",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Rocket Sentry",
+    "Type": "Stratagem",
+    "Category": "Defensive",
+    "Subcategory": "Sentry",
+    "Warbond": "",
+    "InternalName": "rocketsentry",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Liberator",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "AssaultRifle",
+    "Warbond": "",
+    "InternalName": "ar23liberator",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Liberator Penetrator",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "AssaultRifle",
+    "Warbond": "HelldiversMobilize",
+    "InternalName": "ar23pliberatorpenetrator",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Liberator Concussive",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "AssaultRifle",
+    "Warbond": "SteeledVeterans",
+    "InternalName": "ar23cliberatorconcussive",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Adjudicator",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "AssaultRifle",
+    "Warbond": "DemocraticDetonation",
+    "InternalName": "br14adjudicator",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Defender",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "SubmachineGun",
+    "Warbond": "HelldiversMobilize",
+    "InternalName": "smg37defender",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Pummeler",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "SubmachineGun",
+    "Warbond": "PolarPatriots",
+    "InternalName": "smg72pummeler",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Punisher",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "Shotgun",
+    "Warbond": "HelldiversMobilize",
+    "InternalName": "sg8punisher",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Slugger",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "Shotgun",
+    "Warbond": "HelldiversMobilize",
+    "InternalName": "sg8sslugger",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Breaker",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "Shotgun",
+    "Warbond": "HelldiversMobilize",
+    "InternalName": "sg225breaker",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Breaker Incendiary",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "Shotgun",
+    "Warbond": "SteeledVeterans",
+    "InternalName": "sg225iebreakerincendiary",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Breaker Spray & Pray",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "Shotgun",
+    "Warbond": "HelldiversMobilize",
+    "InternalName": "sg225spbreakerspraynpray",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Diligence",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "MarksmanRifle",
+    "Warbond": "HelldiversMobilize",
+    "InternalName": "r63diligence",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Diligence Counter Sniper",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "MarksmanRifle",
+    "Warbond": "HelldiversMobilize",
+    "InternalName": "r63csdiligencecountersniper",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Scythe",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "EnergyWeapon",
+    "Warbond": "HelldiversMobilize",
+    "InternalName": "las5scythe",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Sickle",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "EnergyWeapon",
+    "Warbond": "CuttingEdge",
+    "InternalName": "las16sickle",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Scorcher",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "EnergyWeapon",
+    "Warbond": "HelldiversMobilize",
+    "InternalName": "plas1scorcher",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Punisher Plasma",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "EnergyWeapon",
+    "Warbond": "CuttingEdge",
+    "InternalName": "sg8ppunisherplasma",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Blitzer",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "EnergyWeapon",
+    "Warbond": "CuttingEdge",
+    "InternalName": "arc12blitzer",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Dominator",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "Explosive",
+    "Warbond": "SteeledVeterans",
+    "InternalName": "jar5dominator",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Eruptor",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "Explosive",
+    "Warbond": "DemocraticDetonation",
+    "InternalName": "r36eruptor",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Exploding Crossbow",
+    "Type": "Weapon",
+    "Category": "Primary",
+    "Subcategory": "Explosive",
+    "Warbond": "DemocraticDetonation",
+    "InternalName": "cb9explodingcrossbow",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Peacemaker",
+    "Type": "Weapon",
+    "Category": "Sidearm",
+    "Subcategory": "",
+    "Warbond": "",
+    "InternalName": "p2peacemaker",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Redeemer",
+    "Type": "Weapon",
+    "Category": "Sidearm",
+    "Subcategory": "",
+    "Warbond": "HelldiversMobilize",
+    "InternalName": "p19redeemer",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Grenade Pistol",
+    "Type": "Weapon",
+    "Category": "Sidearm",
+    "Subcategory": "",
+    "Warbond": "DemocraticDetonation",
+    "InternalName": "gp31grenadepistol",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Verdict",
+    "Type": "Weapon",
+    "Category": "Sidearm",
+    "Subcategory": "",
+    "Warbond": "PolarPatriots",
+    "InternalName": "p113verdict",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Senator",
+    "Type": "Weapon",
+    "Category": "Sidearm",
+    "Subcategory": "",
+    "Warbond": "SteeledVeterans",
+    "InternalName": "p4senator",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "High Explosive Grenade",
+    "Type": "Weapon",
+    "Category": "Grenade",
+    "Subcategory": "",
+    "Warbond": "",
+    "InternalName": "highexplosivegrenade",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Frag Grenade",
+    "Type": "Weapon",
+    "Category": "Grenade",
+    "Subcategory": "",
+    "Warbond": "",
+    "InternalName": "fraggrenade",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Impact Grenade",
+    "Type": "Weapon",
+    "Category": "Grenade",
+    "Subcategory": "",
+    "Warbond": "HelldiversMobilize",
+    "InternalName": "impactgrenade",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Smoke Grenade",
+    "Type": "Weapon",
+    "Category": "Grenade",
+    "Subcategory": "",
+    "Warbond": "HelldiversMobilize",
+    "InternalName": "smokegrenade",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Incendiary Grenade",
+    "Type": "Weapon",
+    "Category": "Grenade",
+    "Subcategory": "",
+    "Warbond": "",
+    "InternalName": "incendiarygrenade",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Stun Grenade",
+    "Type": "Weapon",
+    "Category": "Grenade",
+    "Subcategory": "",
+    "Warbond": "",
+    "InternalName": "stungrenade",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Thermite Grenade",
+    "Type": "Weapon",
+    "Category": "Grenade",
+    "Subcategory": "",
+    "Warbond": "",
+    "InternalName": "thermitegrenade",
+    "ImageURL": ""
+  },
+  {
+    "Display Name": "Incendiary Impact",
+    "Type": "Weapon",
+    "Category": "Grenade",
+    "Subcategory": "",
+    "Warbond": "",
+    "InternalName": "incendiaryimpact",
+    "ImageURL": ""
+  }
+]

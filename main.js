641f4b23f79a1bc4053c8b9c8e4d49923f5e2b9c
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2024-05-23 13:50:54 -0400

    turned json into js. added images of stratagems and equipment

---
+++ main.js
@@ -0,0 +1,1 @@
+console.log("main code will go here");
---
+++ stratagemList.js
@@ -0,0 +1,257 @@
+const stratagems = [
+    {
+        name: "Airburst Rocket Launcher",
+        category: "Supply",
+        subcategory: "Weapons"
+    },
+    {
+        name: "Autocannon",
+        category: "Supply",
+        subcategory: "Weapons"
+    },
+    {
+        name: "Expendable Anti-Tank",
+        category: "Supply",
+        subcategory: "Weapons"
+    },
+    {
+        name: "Flamethrower",
+        category: "Supply",
+        subcategory: "Weapons"
+    },
+    {
+        name: "Laser Cannon",
+        category: "Supply",
+        subcategory: "Weapons"
+    },
+    {
+        name: "Stalwart",
+        category: "Supply",
+        subcategory: "Weapons"
+    },
+    {
+        name: "Machine Gun",
+        category: "Supply",
+        subcategory: "Weapons"
+    },
+    {
+        name: "Arc Thrower",
+        category: "Supply",
+        subcategory: "Weapons"
+    },
+    {
+        name: "Grenade Launcher",
+        category: "Supply",
+        subcategory: "Weapons"
+    },
+    {
+        name: "Anti-Material Rifle",
+        category: "Supply",
+        subcategory: "Weapons"
+    },
+    {
+        name: "Railgun",
+        category: "Supply",
+        subcategory: "Weapons"
+    },
+    {
+        name: "Recoilless Rifle",
+        category: "Supply",
+        subcategory: "Weapons"
+    },
+    {
+        name: "Spear",
+        category: "Supply",
+        subcategory: "Weapons"
+    },
+    {
+        name: "Quasar Cannon",
+        category: "Supply",
+        subcategory: "Weapons"
+    },
+    {
+        name: "Heavy Machine Gun",
+        category: "Supply",
+        subcategory: "Weapons"
+    },
+    {
+        name: "Guard Dog Laser",
+        category: "Supply",
+        subcategory: "Backpacks"
+    },
+    {
+        name: "Guard Dog Bullets",
+        category: "Supply",
+        subcategory: "Backpacks"
+    },
+    {
+        name: "Jump Pack",
+        category: "Supply",
+        subcategory: "Backpacks"
+    },
+    {
+        name: "Supply Pack",
+        category: "Supply",
+        subcategory: "Backpacks"
+    },
+    {
+        name: "Shield Generator Pack",
+        category: "Supply",
+        subcategory: "Backpacks"
+    },
+    {
+        name: "Ballistic Shield Backpack",
+        category: "Supply",
+        subcategory: "Backpacks"
+    },
+    {
+        name: "Patriot Exosuit",
+        category: "Supply",
+        subcategory: "Vehicles"
+    },
+    {
+        name: "Eagle Strafing Run",
+        category: "Offensive",
+        subcategory: "Eagle"
+    },
+    {
+        name: "Eagle Airstrike",
+        category: "Offensive",
+        subcategory: "Eagle"
+    },
+    {
+        name: "Eagle Cluster Bomb",
+        category: "Offensive",
+        subcategory: "Eagle"
+    },
+    {
+        name: "Eagle Napalm Strike",
+        category: "Offensive",
+        subcategory: "Eagle"
+    },
+    {
+        name: "Eagle Smoke Strike",
+        category: "Offensive",
+        subcategory: "Eagle"
+    },
+    {
+        name: "Eagle 110mm Rocket Pods",
+        category: "Offensive",
+        subcategory: "Eagle"
+    },
+    {
+        name: "Eagle 500kg Bomb",
+        category: "Offensive",
+        subcategory: "Eagle"
+    },
+    {
+        name: "Orbital Precision Strike",
+        category: "Offensive",
+        subcategory: "Orbital"
+    },
+    {
+        name: "Orbital Airburst Strike",
+        category: "Offensive",
+        subcategory: "Orbital"
+    },
+    {
+        name: "Orbital 120mm HE Barrage",
+        category: "Offensive",
+        subcategory: "Orbital"
+    },
+    {
+        name: "Orbital 380mm HE Barrage",
+        category: "Offensive",
+        subcategory: "Orbital"
+    },
+    {
+        name: "Orbital Walking Barrage",
+        category: "Offensive",
+        subcategory: "Orbital"
+    },
+    {
+        name: "Orbital Laser",
+        category: "Offensive",
+        subcategory: "Orbital"
+    },
+    {
+        name: "Orbital Railcannon Strike",
+        category: "Offensive",
+        subcategory: "Orbital"
+    },
+    {
+        name: "Orbital Gatling Barrage",
+        category: "Offensive",
+        subcategory: "Orbital"
+    },
+    {
+        name: "Orbital Gas Strike",
+        category: "Offensive",
+        subcategory: "Orbital"
+    },
+    {
+        name: "Orbital EMS Strike",
+        category: "Offensive",
+        subcategory: "Orbital"
+    },
+    {
+        name: "Orbital Smoke Strike",
+        category: "Offensive",
+        subcategory: "Orbital"
+    },
+    {
+        name: "Tesla Tower",
+        category: "Defensive",
+        subcategory: "Emplacement"
+    },
+    {
+        name: "Mortar Sentry",
+        category: "Defensive",
+        subcategory: "Sentry"
+    },
+    {
+        name: "EMS Mortar Sentry",
+        category: "Defensive",
+        subcategory: "Sentry"
+    },
+    {
+        name: "Machine Gun Sentry",
+        category: "Defensive",
+        subcategory: "Sentry"
+    },
+    {
+        name: "Gatling Sentry",
+        category: "Defensive",
+        subcategory: "Sentry"
+    },
+    {
+        name: "Anti-Personnel Minefield",
+        category: "Defensive",
+        subcategory: "Emplacement"
+    },
+    {
+        name: "Incendiary Mines",
+        category: "Defensive",
+        subcategory: "Emplacement"
+    },
+    {
+        name: "Shield Generator Relay",
+        category: "Defensive",
+        subcategory: "Emplacement"
+    },
+    {
+        name: "HMG Emplacement",
+        category: "Defensive",
+        subcategory: "Emplacement"
+    },
+    {
+        name: "Autocannon Sentry",
+        category: "Defensive",
+        subcategory: "Sentry"
+    },
+    {
+        name: "Rocket Sentry",
+        category: "Defensive",
+        subcategory: "Sentry"
+    }
+];

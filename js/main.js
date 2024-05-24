c28174dfa2ab3e92d6769d031b0af1551654b78b
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2024-05-24 14:48:51 -0400

    got random strats working and showing up

--- js/main.js
+++ js/main.js
@@ -2,23 +2,38 @@ const stratagemsContainer = document.getElementById("stratagemsContainer");
 const equipmentContainer = document.getElementById("equipmentContainer");
 
 const rollStratagems = () => {
-    // get four random numbers that arent the same and get the strats at those indices
+    // get random numbers that arent the same and get the strats at those indices
     stratagemsContainer.innerHTML = "";
-    const fourRandomUniqueNumbers = getFourRandomUniqueNumbers();
+    const randomUniqueNumbers = getRandomUniqueNumbers();
 
-    // console.log(stratagemsList[randomStratagemIndex]);
-    // stratagemsContainer.innerHTML = `
-
-    // `;
+    for (let i = 0; i < randomUniqueNumbers.length; i++) {
+        console.log(stratagemsList[randomUniqueNumbers[i]]);
+        const stratagem = stratagemsList[randomUniqueNumbers[i]];
+        stratagemsContainer.innerHTML += `
+          <div class="col-3 d-flex justify-content-center">
+            <div class="card">
+              <img
+                  src="./images/stratagems/${stratagem.imageURL}"
+                  class="img-card-top"
+                  alt="${stratagem.displayName}"
+              />
+              <div class="card-body stratagemNameContainer align-items-center">
+                  <p class="card-title">${stratagem.displayName}</p>
+              </div>
+            </div>
+          </div>
+        `;
+    }
 };
 
-const getFourRandomUniqueNumbers = () => {
+const getRandomUniqueNumbers = (max = 4) => {
     let numbers = [];
     let randomNumber = null;
-    while (numbers.length < 4) {
+    while (numbers.length < max) {
         randomNumber = Math.floor(Math.random() * stratagemsList.length);
         if (!numbers.includes(randomNumber)) {
             numbers.push(randomNumber);
         }
     }
+    return numbers;
 };

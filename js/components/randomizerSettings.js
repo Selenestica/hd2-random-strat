ee67e006c3c277d8f2201cd62636919feaef0690
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2024-10-26 16:47:41 -0400

    implementing local storage code and some refactoring

---
+++ js/components/randomizerSettings.js
@@ -0,0 +1,115 @@
+// const eaglesAndOrbitalsOptions = document.getElementById(
+//     "eaglesAndOrbitalsOptions"
+// );
+// const defenseAndSupplyOptions = document.getElementById(
+//     "defenseAndSupplyOptions"
+// );
+
+const TOP_ROW = ["Eagle", "Orbital"]
+const BOTTOM_ROW = ["Defense", "Supply"]
+
+const genSettingsRadios = () => {
+    
+};
+
+genSettingsRadios();
+
+    <div class="col">
+        <h5 class="text-white my-2">Eagles</h5>
+        <div>
+            <div class="form-check">
+                <input
+                    class="form-check-input"
+                    type="radio"
+                    name="Eagle"
+                    id="onlyEaglesRadio"
+                    value="onlyEagles"
+                />
+                <label
+                    class="text-white form-check-label"
+                    for="onlyEaglesRadio"
+                >
+                    Only Eagles
+                </label>
+            </div>
+            <div class="form-check">
+                <input
+                    class="form-check-input"
+                    type="radio"
+                    name="Eagle"
+                    id="noEaglesRadio"
+                    value="noEagles"
+                />
+                <label class="text-white form-check-label" for="noEaglesRadio">
+                    No Eagles
+                </label>
+            </div>
+            <div class="form-check">
+                <input
+                    class="form-check-input"
+                    type="radio"
+                    name="Eagle"
+                    id="defaultEaglesRadio"
+                    value="defaultEagles"
+                    checked
+                />
+                <label
+                    class="text-white form-check-label"
+                    for="defaultEaglesRadio"
+                >
+                    Default
+                </label>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <h5 class="text-white my-2">Orbitals</h5>
+        <div>
+            <div class="form-check">
+                <input
+                    class="form-check-input"
+                    type="radio"
+                    name="Orbital"
+                    id="onlyOrbitalsRadio"
+                    value="onlyOrbital"
+                />
+                <label
+                    class="text-white form-check-label"
+                    for="onlyOrbitalsRadio"
+                >
+                    Only Orbitals
+                </label>
+            </div>
+            <div class="form-check">
+                <input
+                    class="form-check-input"
+                    type="radio"
+                    name="Orbital"
+                    id="noOrbitalsRadio"
+                    value="noOrbitals"
+                />
+                <label
+                    class="text-white form-check-label"
+                    for="noOrbitalsRadio"
+                >
+                    No Orbitals
+                </label>
+            </div>
+            <div class="form-check">
+                <input
+                    class="form-check-input"
+                    type="radio"
+                    name="Orbital"
+                    id="defaultOrbitalsRadio"
+                    value="defaultOrbitals"
+                    checked
+                />
+                <label
+                    class="text-white form-check-label"
+                    for="defaultOrbitalsRadio"
+                >
+                    Default
+                </label>
+            </div>
+        </div>
+    </div>

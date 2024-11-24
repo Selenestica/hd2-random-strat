f92eaba43f778acd3976420719eba2b875de5c88
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2024-11-23 20:56:56 -0500

    added armor passives constant

---
+++ js/constants/armorPassives.js
@@ -0,0 +1,14 @@
+const armorPassives = [
+    "Advanced Filtration",
+    "Democracy Protects",
+    "Electrical Conduit",
+    "Engineering Kit",
+    "Extra Padding",
+    "Fortified",
+    "Inflammable",
+    "Med-Kit",
+    "Peak Physique",
+    "Scout",
+    "Servo-Assisted",
+    "Unflinching"
+];

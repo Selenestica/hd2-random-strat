aefd74ec37dc467010faa1190cdc560684a24909
JoeBen Wilson <53917177+Selenestica@users.noreply.github.com>
2024-11-27 11:43:23 -0500

    Update head.js
    work on abstracting boiler plate

--- js/components/head.js
+++ js/components/head.js
@@ -1,1 +1,24 @@
-const page = "here"
+const page = document.getElementById("html")
+page.innerHTML += `    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+            crossorigin="anonymous"
+        />
+
+        <link rel="stylesheet" type="text/css" href="./css/custom.css" />
+        <link
+            rel="shortcut icon"
+            type="image/png"
+            href="./images/stratagems/orbitalprecisionstrike.png"
+        />
+        <meta
+            name="description"
+            content="Prove that you have the Strength and the Courage to dispense freedom with Super Earth's surplus arsenal!"
+        />
+        <title>Helldivers 2 Randomizer</title>
+    </head>`

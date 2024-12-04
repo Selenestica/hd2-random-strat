6338f3cb729af72f91b2aa935dfab11b4c3717c4
JoeBen Wilson <53917177+Selenestica@users.noreply.github.com>
2024-12-04 13:17:18 -0500

    Update footer.js

--- js/components/footer.js
+++ js/components/footer.js
@@ -2,6 +2,6 @@ const footer = document.getElementById("footer")
 footer.innerHTML += `
         <div class="row d-flex justify-content-center py-5 bg-dark">
             <div class="col-8 text-center">
-                <p class="text-monospace" id="credits">Designed & Built by JoeBen Wilson</p>
+                <p class="text-monospace" id="credits">Designed & Built by <a href="https://www.joebenwilson.com" target="_blank">JoeBen Wilson</a></p>
             </div>
         </div>`

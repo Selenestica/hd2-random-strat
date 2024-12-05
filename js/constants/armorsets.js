a2df7804745f0792de754fae8868825c9415d57a
JoeBen Wilson <53917177+Selenestica@users.noreply.github.com>
2024-12-05 13:07:02 -0500

    Update armorsets.js

--- js/constants/armorsets.js
+++ js/constants/armorsets.js
@@ -59,4 +59,54 @@ const ARMOR_SETS = [
         internalName: "cm21trenchparamedic",
         imageURL: "cm21trenchparamedic.png"
     },
+    {
+        displayName: "CW-4 Arctic Ranger",
+        type: "Equipment",
+        category: "armor",
+        tags: ["Light"],
+        warbond: "Polar Patriots",
+        warbondCode: "warbond5",
+        internalName: "c4arcticranger",
+        imageURL: "c4arcticranger.png"
+    },
+    {
+        displayName: "EX-00 Prototype X",
+        type: "Equipment",
+        category: "armor",
+        tags: ["Light"],
+        warbond: "Cutting Edge",
+        warbondCode: "warbond4",
+        internalName: "ex00prototypex",
+        imageURL: "ex00prototypex.png"
+    },
+    {
+        displayName: "FS-37 Ravager",
+        type: "Equipment",
+        category: "armor",
+        tags: ["Light"],
+        warbond: "Superstore",
+        warbondCode: "superstore",
+        internalName: "fs37ravager",
+        imageURL: "fs37ravager.png"
+    },
+    {
+        displayName: "FS-38 Eradicator",
+        type: "Equipment",
+        category: "armor",
+        tags: ["Light"],
+        warbond: "Superstore",
+        warbondCode: "superstore",
+        internalName: "fs38eradicator",
+        imageURL: "fs38eradicator.png"
+    },
+    {
+        displayName: "I-09 Heatseeker",
+        type: "Equipment",
+        category: "armor",
+        tags: ["Light"],
+        warbond: "Freedom's Flame",
+        warbondCode: "warbond7",
+        internalName: "i09heatseeker",
+        imageURL: "i09heatseeker.png"
+    },
 ]

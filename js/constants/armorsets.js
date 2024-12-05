92b0d31bcdd5f8f8252d3e6b5c8cd39738a3a022
JoeBen Wilson <53917177+Selenestica@users.noreply.github.com>
2024-12-05 09:06:50 -0500

    Update armorsets.js

--- js/constants/armorsets.js
+++ js/constants/armorsets.js
@@ -1,1 +1,32 @@
-const ARMOR_SETS = []
+const ARMOR_SETS = [
+    {
+        displayName: "AF-50 Noxious Ranger",
+        type: "Equipment",
+        category: "armor",
+        tags: ["Light"],
+        warbond: "Chemical Agents",
+        warbondCode: "warbond8",
+        internalName: "af50noxiousranger",
+        imageURL: "af50noxiousranger.png"
+    },
+    {
+        displayName: "B-08 Light Gunner",
+        type: "Equipment",
+        category: "armor",
+        tags: ["Light"],
+        warbond: "Superstore",
+        warbondCode: "superstore",
+        internalName: "b08lightgunner",
+        imageURL: "b08lightgunner.png"
+    },
+    {
+        displayName: "CE-07 Demolition Specialist",
+        type: "Equipment",
+        category: "armor",
+        tags: ["Light"],
+        warbond: "Democratic Detonation",
+        warbondCode: "warbond6",
+        internalName: "ce07demolitionspecialist",
+        imageURL: "ce07demolitionspecialist.png"
+    },
+]

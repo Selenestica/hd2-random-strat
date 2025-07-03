e4114a0c938b23b0235064c8010147ea36fd7fa4
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2025-07-03 08:07:54 -0400

    fixed max samples due to samples scanner

--- js/utils/getMaxSuperSamplesForMission.js
+++ js/utils/getMaxSuperSamplesForMission.js
@@ -1,14 +1,14 @@
 const getMaxSuperSamplesForMission = () => {
   if (missionCounter < 11) {
-    return 3;
-  } else if (missionCounter < 14) {
-    return 4;
-  } else if (missionCounter < 17) {
-    return 5;
-  } else if (missionCounter < 20) {
     return 6;
+  } else if (missionCounter < 14) {
+    return 8;
+  } else if (missionCounter < 17) {
+    return 10;
+  } else if (missionCounter < 20) {
+    return 12;
   }
-  return 7;
+  return 14;
 };
 
 // 3, 4, 5, 6, 7

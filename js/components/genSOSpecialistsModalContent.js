00c5ed37cbc5808e1a8a0944b54cfd493f134215
JoeBen Wilson <53917177+Selenestica@users.noreply.github.com>
2025-07-15 10:50:53 -0400

    specialist modal progress

---
+++ js/components/genSOSpecialistsModalContent.js
@@ -0,0 +1,51 @@
+const specialistsList = document.getElementById("specialistsList");
+
+let specialists = [...SPECOPSSPECS];
+
+const genSpecialistItemNames = (spec, type) => {
+  let elementsList = "";
+  let list = spec.stratagems;
+  if (type === "equip") {
+    list = [spec.primary, spec.secondary, spec.throwable, spec.armorPassive];
+  }
+  for (let i = 0; i < list.length; i++) {
+    elementsList += `<li class="text-white">${list[i]}</li>`;
+  }
+  return elementsList;
+};
+
+const genSpecialistCard = (spec, i) => {
+  return `
+      <div class="card col-lg-3 col-sm-12 specialistCards m-2" id="soSpecialistCard${i}" onclick="${
+    spec.locked ? "" : `setSpecialist('${i}')`
+  }">
+        <div class="card-header">
+          <h5 class="text-white specialistHeadersClass" id="specialistHeader${i}">${
+    spec.locked ? "Locked" : spec.displayName
+  }</h5>
+        </div>
+        <div class="card-body ${spec.locked ? "text-center" : ""}">
+          ${
+            spec.locked
+              ? `<i class="fa-solid text-white fa-3x fa-question"></i>`
+              : `
+          <p class="text-white mb-0">Stratagems:</p>
+          <ul>
+            ${genSpecialistItemNames(spec, "strat")}
+          </ul>
+          <p class="text-white mb-0">Equipment:</p>
+          <ul>
+            ${genSpecialistItemNames(spec, "equip")}
+          </ul>`
+          }
+        </div>
+    `;
+};
+
+const genSOSpecialistsModalContent = () => {
+  // create specialist cards and add them to an array
+  for (let i = 0; i < specialists.length; i++) {
+    const card = genSpecialistCard(specialists[i], i);
+    specialistsList.innerHTML += card;
+  }
+};

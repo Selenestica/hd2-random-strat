ef18bd9cd986001f2d11dd834ab53c0223caa64f
JoeBen Wilson <53917177+Selenestica@users.noreply.github.com>
2025-07-16 11:36:45 -0400

    hide the link for now

--- js/components/genChallengeCards.js
+++ js/components/genChallengeCards.js
@@ -22,12 +22,12 @@ const genChallengeCards = () => {
       icon: "fa-sack-dollar",
       iconClasses: "text-success",
     },
-    {
-      displayName: "Special Ops",
-      internalName: "specialops",
-      icon: "fa-person-rifle",
-      iconClasses: "text-primary",
-    },
+    // {
+    //   displayName: "Special Ops",
+    //   internalName: "specialops",
+    //   icon: "fa-person-rifle",
+    //   iconClasses: "text-primary",
+    // },
   ];
 
   for (let i = 0; i < challenges.length; i++) {

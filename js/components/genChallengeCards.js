d7cef383f5129ebf04171d74229cdf970f83edad
JoeBen Wilson <53917177+Selenestica@users.noreply.github.com>
2025-07-24 10:31:58 -0400

    added special ops to main page. edited main page styles a bit

--- js/components/genChallengeCards.js
+++ js/components/genChallengeCards.js
@@ -22,19 +22,19 @@ const genChallengeCards = () => {
       icon: "fa-sack-dollar",
       iconClasses: "text-success",
     },
-    // {
-    //   displayName: "Special Ops",
-    //   internalName: "specialops",
-    //   icon: "fa-person-rifle",
-    //   iconClasses: "text-primary",
-    // },
+    {
+      displayName: "Special Ops",
+      internalName: "specialops",
+      icon: "fa-person-rifle",
+      iconClasses: "text-primary",
+    },
   ];
 
   for (let i = 0; i < challenges.length; i++) {
     const challenge = challenges[i];
     const card = document.createElement("a");
     card.href = `./${challenge.internalName}`;
-    card.className = `card col-5 bg-none m-1 text-center challengeCards`;
+    card.className = `card col-5 bg-none m-1 p-2 text-center challengeCards`;
     card.innerHTML = `
     <i class="${
       challenge.iconClasses ? challenge.iconClasses : "text-danger"

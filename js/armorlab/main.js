67017e96a4112c32d6e3223268b7dc10e5548049
JoeBen Wilson <53917177+Selenestica@users.noreply.github.com>
2025-09-11 13:53:35 -0400

    fixed broken paths to some images

--- js/armorlab/main.js
+++ js/armorlab/main.js
@@ -1,42 +1,52 @@
 const helmetsList = document.getElementById("helmetsList");
+const armorList = document.getElementById("armorList");
+const capesList = document.getElementById("capesList");
 
-const genImageCards = async () => {
+const genImageModalContent = async () => {
   // will need to refactor armor sets and armor passives images into separate files
   //   const paths = ["armor", "helmets", "capes"];
+  for (let i = 0; i < HELMETS.length; i++) {
+    const helmetCard = await generateItemCard(HELMETS[i], "helmets");
+    helmetsList.innerHTML += helmetCard;
+  }
+  for (let j = 0; j < ARMOR_SETS.length; j++) {
+    const armorCard = await generateItemCard(ARMOR_SETS[j], "armor");
+    armorList.innerHTML += armorCard;
+  }
+  for (let i = 0; i < CAPES.length; i++) {
+    const capeCard = await generateItemCard(CAPES[i], "capes");
+    capesList.innerHTML += capeCard;
+  }
 };
 
-const generateItemCard = (
-  item,
-  inModal,
-  imgDir,
-  currentItemIndex = null,
-  type = null,
-  missionFailed = false
-) => {
+const generateItemCard = (item, type) => {
   // display the item image in the modal or accordion item
-  let mandatoryStratStyle = getMandatoryStratStyle(item.displayName);
   let style = "col-2";
-  let modalTextStyle = "pcItemCardText";
-  let fcn = "";
-  let typeText = "";
-  if (inModal) {
-    style = "pcModalItemCards col-6";
-    modalTextStyle = "";
-    fcn = !missionFailed
-      ? `claimItem(${currentItemIndex})`
-      : `claimPunishment(${currentItemIndex})`;
-    typeText = `<p class="card-title fst-italic text-white">${type}</p>`;
+  if (type === "helmets") {
+    console.log("helmet");
   }
+  if (type === "capes") {
+    console.log("cape");
+  }
+  if (type === "armor") {
+    console.log("armor");
+  }
+  style = "pcModalItemCards col-1";
   return `
-    <div onclick="${fcn}" class="card ${mandatoryStratStyle} d-flex ${style} pcNoHoverItemCards mx-1">
-    ${typeText}
+    <div onclick="setItem(${item.internalName}, ${type})" class="card d-flex ${style} pcItemCards mx-1">
       <img
-          src="../images/${imgDir}/${item.imageURL}"
+          src="../images/${type}/${item.internalName}.webp"
           class="img-card-top"
           alt="${item.displayName}"
       />
-      <div class="card-body itemNameContainer p-0 p-lg-2 align-items-center">
-          <p class="card-title text-white ${modalTextStyle}">${item.displayName}</p>
+      <div class="card-body itemNameContainer p-0 align-items-center">
+          <p class="card-title text-white">${item.displayName}</p>
       </div>
     </div>`;
 };
+
+const setItem = (name, type) => {
+  console.log(name, type);
+};
+
+genImageModalContent();

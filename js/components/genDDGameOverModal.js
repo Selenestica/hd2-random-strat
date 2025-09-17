a6a2ee872a1efb826e3cc420e081de826d4b66e6
JoeBen Wilson <53917177+Selenestica@users.noreply.github.com>
2025-09-17 14:50:11 -0400

    added flavor text to game over modal for Debt Divers

--- js/components/genDDGameOverModal.js
+++ js/components/genDDGameOverModal.js
@@ -52,6 +52,10 @@ const genDDGameOverModal = async () => {
   const totalScore = parScore + totalMissionTimeRemaining - failedMissions * 50;
   superSamplesCollected += highValueItemsCollected * 2;
 
+  ddGameOverModalBody.innerHTML += `<p class="mb-0 text-white">Congratulations, Helldiver! You have reduced your debt. Your <b>creditors have taken 1000 Super Credits</b> from your balance.</p>`;
+  ddGameOverModalBody.innerHTML += `<p class="mb-0 text-white">If you wish to continue playing (i.e. to help your teammates pay off their debt) simply close this screen and keep playing as you normally would. Your score has been saved and will be unaffected by any more progress you make on this save file.</p>`;
+
+  ddGameOverModalBody.innerHTML += `<p class="mb-0 text-white"><br /></p>`;
   ddGameOverModalBody.innerHTML += `<h5 class="text-white">Breakdown:</h5>`;
   ddGameOverModalBody.innerHTML += `<p class="mb-0 text-white">Difficulty: ${difficulty}</p>`;
   ddGameOverModalBody.innerHTML += `<p class="mb-0 text-white">Start Time: ${dateStarted}</p>`;

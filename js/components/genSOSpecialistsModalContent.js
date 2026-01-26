61f9fa8191c729e31501f21c6576ab60ad83ae59
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2026-01-25 19:51:03 -0500

    fix to specialist modal in Special Ops

--- js/components/genSOSpecialistsModalContent.js
+++ js/components/genSOSpecialistsModalContent.js
@@ -1,13 +1,22 @@
-const specialistsList = document.getElementById("specialistsList");
-const amountOfSpecsUnlockedText = document.getElementById(
-  "amountOfSpecsUnlockedText"
-);
+const specialistsList = document.getElementById('specialistsList');
+const amountOfSpecsUnlockedText = document.getElementById('amountOfSpecsUnlockedText');
 
 const genSpecialistItemNames = (spec, type) => {
-  let elementsList = "";
-  let list = spec.stratagems;
-  if (type === "equip") {
-    list = [spec.primary, spec.secondary, spec.throwable, spec.armorPassive];
+  let elementsList = '';
+  let list = [];
+  if (type === 'strat') {
+    for (let j = 0; j < spec.stratagems.length; j++) {
+      list.push(stratagems[spec.stratagems[j]].displayName);
+    }
+  }
+  if (type === 'equip') {
+    const { primary, secondary, throwable, armorPassive } = spec;
+    list = [
+      primaries[primary].displayName,
+      secondaries[secondary].displayName,
+      throwables[throwable].displayName,
+      armorPassives[armorPassive].displayName,
+    ];
   }
   for (let i = 0; i < list.length; i++) {
     elementsList += `<li class="text-white">${list[i]}</li>`;
@@ -18,33 +27,33 @@ const genSpecialistItemNames = (spec, type) => {
 const genSpecialistCard = (spec, i, currentSpec, latestSpec) => {
   let displayName = spec.displayName;
   if (spec.locked) {
-    displayName = "Locked";
+    displayName = 'Locked';
   }
   if (currentSpec.displayName === spec.displayName) {
-    displayName += " (Current)";
+    displayName += ' (Current)';
   }
   if (latestSpec.displayName === spec.displayName) {
     displayName += `<i class="bi bi-key-fill mx-2 text-warning"></i>`;
   }
   return `
       <div class="card col-lg-3 col-sm-12 specialistCards m-2" id="soSpecialistCard${i}" onclick="${
-    spec.locked ? "" : `setSpecialist('${i}')`
-  }">
+        spec.locked ? '' : `setSpecialist('${i}')`
+      }">
         <div class="card-header">
           <h5 class="text-white specialistHeadersClass" id="specialistHeader${i}">${displayName}</h5>
         </div>
-        <div class="card-body ${spec.locked ? "text-center" : ""}">
+        <div class="card-body ${spec.locked ? 'text-center' : ''}">
           ${
             spec.locked
               ? `<i class="bi bi-question-lg text-white fa-3x"></i>`
               : `
           <p class="text-white mb-0">Stratagems:</p>
           <ul>
-            ${genSpecialistItemNames(spec, "strat")}
+            ${genSpecialistItemNames(spec, 'strat')}
           </ul>
           <p class="text-white mb-0">Equipment:</p>
           <ul>
-            ${genSpecialistItemNames(spec, "equip")}
+            ${genSpecialistItemNames(spec, 'equip')}
           </ul>`
           }
         </div>
@@ -52,15 +61,10 @@ const genSpecialistCard = (spec, i, currentSpec, latestSpec) => {
 };
 
 const genSOSpecialistsModalContent = (currSpecialist, latestSpecialist) => {
-  specialistsList.innerHTML = "";
+  specialistsList.innerHTML = '';
   // create specialist cards and add them to an array
   for (let i = 0; i < specialists.length; i++) {
-    const card = genSpecialistCard(
-      specialists[i],
-      i,
-      currSpecialist,
-      latestSpecialist
-    );
+    const card = genSpecialistCard(specialists[i], i, currSpecialist, latestSpecialist);
     specialistsList.innerHTML += card;
   }
 
